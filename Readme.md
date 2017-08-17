f2d0ec3fa96376b7b1c15e94a82be05ad768b7ff
Ye Jia <ye.jia.alice@gmail.com>
2017-08-17 21:39:25 +0800

    pass 7

--- Readme.md
+++ Readme.md
@@ -5,3 +5,5 @@
 ### 目的
 
 > 学会插件系统 [tapable](https://github.com/webpack/tapable) ，并根据插件系统，定制出自己的请求库
+
+作业2提交 上海小分队三组 Alice小叶
--- karma.conf.js
+++ karma.conf.js
@@ -12,7 +12,11 @@ module.exports = function(config) {
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['mocha'],
 
-
+    // client: {
+    //   mocha: {
+    //     timeout: 6000
+    //   }
+    // },
     // list of files / patterns to load in the browser
     files: [
       'test/**.js'
@@ -52,7 +56,6 @@ module.exports = function(config) {
 
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
-
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
