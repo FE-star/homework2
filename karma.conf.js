62e0ca9230240b4b2e2cf89eb88531a9a90a5f64
favers <favers0601@gmail.com>
2018-02-08 10:44:49 +0800

    add chrome

--- karma.conf.js
+++ karma.conf.js
@@ -42,7 +42,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
+    browsers: process.env.TRAVIS ? ['Chrome_travis_ci'] : ['Chrome'],
 
     customLaunchers: {
       Chrome_travis_ci: {
