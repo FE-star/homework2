03db277f4bf125bbf9fcb0df0fed2c79fabfc614
machock <229235844@qq.com>
2018-06-27 23:14:36 +0800

    try to connect travis CI

--- karma.conf.js
+++ karma.conf.js
@@ -56,7 +56,15 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
+    // browsers: ['Chrome'],
+    browsers: ['ChromeHeadlessNoSandbox'],
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    },
+
 
     webpack: {
       resolve: {
