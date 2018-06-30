20fff8f97e854a47e2b9e0fa7470b7d350189bfc
erica <erica.zhang@zhajob.com>
2018-06-30 15:13:23 +0800

    homework2 finished

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,5 @@
 // Karma configuration
 // Generated on Fri Aug 11 2017 22:02:05 GMT+0800 (CST)
-
 module.exports = function(config) {
   config.set({
 
@@ -57,7 +56,7 @@ module.exports = function(config) {
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: ['Chrome'],
-
+    
     webpack: {
       resolve: {
         extensions: ['.js']
