f0289ac279aeea2530ea8c8dadf82672c5cdb5d4
asd2766 <270092797@qq.com>
2018-07-07 22:49:29 +0800

    update karma

--- karma.conf.js
+++ karma.conf.js
@@ -56,7 +56,13 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
+    browsers: ['Chrome', 'ChromeHeadless'],
+    customLaunchers: {
+      Chrome_travis_ci: {
+        base: 'Chrome',
+        flags: ['--no-sandbox']
+      }
+    },
 
     webpack: {
       resolve: {
