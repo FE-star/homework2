723a0dbf00e3143ea997001754c78d28cc0ee762
九酒 <jiujiu@jiujiudeMacBook-Pro.local>
2018-11-13 22:00:17 +0800

    karma conf

--- karma.conf.js
+++ karma.conf.js
@@ -71,7 +71,7 @@ module.exports = function(config) {
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: true,//!!process.env.TRAVIS,
+    singleRun: !!process.env.TRAVIS,
 
     // Concurrency level
     // how many browser should be started simultaneous
