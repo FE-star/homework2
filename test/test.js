af358312f6e2c38629bf8e3897357ba69d6ce8f8
chenli <chenl@homeking365.com>
2022-08-20 23:59:58 +0800

    feat: 完善用例,并提交作业

--- test/test.js
+++ test/test.js
@@ -17,8 +17,8 @@ describe('DB', function () {
       constructor(options) {
         super(options)
 
-        this.plugin('endpoint', function () {
-          return new Promise((resolve) => {
+        this.hooks.endpoint.tapPromise('endpoint', function () {
+          return new Promise(resolve => {
             setTimeout(() => {
               resolve({ retcode: 0, res: { msg: 'hello world' } })
             }, 0)
@@ -27,32 +27,43 @@ describe('DB', function () {
       }
     }
 
-    const xx = new XX()
-    xx.request()
-      .then((res) => {
-        assert.equal(res.res.msg, 'hello world')
-        done()
-      })
+    const xx = new XX({})
+    xx.request().then(res => {
+      assert.equal(res.res.msg, 'hello world')
+      done()
+    })
   })
 
   it('可以根据不同的options，使用不同的endpoint', function (done) {
     class AA extends DB {
       constructor(options) {
         super(options)
-        this.plugin('endpoint', function (options) {
+        this.hooks.endpoint.tapPromise('endpoint', function (options) {
           if (options.type === 1) {
-            return new Promise((resolve) => {
+            return new Promise(resolve => {
               setTimeout(() => {
                 resolve({ retcode: 1, msg: 'logout' })
               }, 0)
             })
+          } else {
+            return new Promise(resolve => {
+              setTimeout(() => {
+                resolve(options)
+              }, 0)
+            })
           }
         })
-        this.plugin('endpoint', function (options) {
+        this.hooks.endpoint.tapPromise('endpoint', function (options) {
           if (options.type === 0) {
-            return new Promise((resolve) => {
+            return new Promise(resolve => {
               setTimeout(() => {
                 resolve({ retcode: 0, res: { msg: 'hello world' } })
+              }, 0)
+            })
+          } else {
+            return new Promise(resolve => {
+              setTimeout(() => {
+                resolve(options)
               }, 0)
             })
           }
@@ -60,14 +71,15 @@ describe('DB', function () {
       }
     }
 
-    const aa = new AA
+    const aa = new AA()
     // 如果 options.type === 1，则返回第一个答案
     aa.request({ type: 1 })
       .then(res => {
         assert.equal(res.retcode, 1)
         // 如果 options.type === 0，则返回第二个答案
         return aa.request({ type: 0 })
-      }).then(res => {
+      })
+      .then(res => {
         assert.equal(res.retcode, 0)
         done()
       })
@@ -77,12 +89,12 @@ describe('DB', function () {
     class YY extends DB {
       constructor(options) {
         super(options)
-        this.plugin('options', (options) => {
+        this.hooks.options.tap('options', options => {
           // modify options
           options.flag = true
           return options
         })
-        this.plugin('endpoint', (options) => {
+        this.hooks.endpoint.tapPromise('endpoint', options => {
           // init
           assert.equal(options.init, true)
           // merge
@@ -100,31 +112,30 @@ describe('DB', function () {
     }
 
     const yy = new YY({ init: true })
-    yy.request({ url: 'my://hello' })
-      .then((res) => {
-        done()
-      })
+    yy.request({ url: 'my://hello' }).then(res => {
+      done()
+    })
   })
 
   it('可以设置多个options插件', function (done) {
     class BB extends DB {
       constructor(options) {
         super(options)
-        this.plugin('options', (options) => {
+        this.hooks.options.tap('options', options => {
           // modify options
           options.flag = true
           return options
         })
-        this.plugin('options', (options) => {
+        this.hooks.options.tap('options', options => {
           // modify options，后面的覆盖前面的
           options.flag = false
-          return options 
+          return options
         })
-        this.plugin('options', (options) => {
+        this.hooks.options.tap('options', options => {
           options.url = 'you://hello'
           return options
         })
-        this.plugin('endpoint', (options) => {
+        this.hooks.endpoint.tapPromise('endpoint', options => {
           // init
           assert.equal(options.init, true)
           // merge
@@ -142,60 +153,80 @@ describe('DB', function () {
     }
 
     const bb = new BB({ init: true })
-    bb.request({ url: 'my://hello' })
-      .then((res) => {
-        done()
-      })
+    bb.request({ url: 'my://hello' }).then(res => {
+      done()
+    })
   })
 
   it('可以通过judge插件判断返回是否正确', function (done) {
     class CC extends DB {
       constructor(options) {
         super(options)
-        this.plugin('endpoint', function (options) {
+
+        this.hooks.endpoint.tapPromise('endpoint', function (options) {
           if (options.type === 1) {
-            return new Promise((resolve) => {
+            return new Promise(resolve => {
               setTimeout(() => {
                 resolve({ retcode: 1, msg: 'logout' })
               }, 0)
             })
-          }
-        })
-        this.plugin('endpoint', function (options) {
-          if (options.type === 0) {
-            return new Promise((resolve) => {
+          } else {
+            return new Promise(resolve => {
               setTimeout(() => {
-                resolve({ retcode: 0, res: { msg: 'hello world' } })
+                resolve(options)
               }, 0)
             })
           }
         })
 
-        this.plugin('judge', function (res) {
-          if (res.retcode !== 0) return true
+        this.hooks.endpoint.tapPromise('endpoint', function (options) {
+          if (options.type === 0) {
+            return new Promise(resolve => {
+              setTimeout(() => {
+                resolve({ retcode: 0, res: { msg: 'hello world' } })
+              }, 0)
+            })
+          } else {
+            return new Promise(resolve => {
+              setTimeout(() => {
+                resolve(options)
+              }, 0)
+            })
+          }
+        })
+
+        this.hooks.judge.tap('judge', function (res) {
+          return res.retcode === 0
         })
       }
     }
 
-    const cc = new CC
+    const cc = new CC()
     cc.request({ type: 0 })
-      .then((res) => {
+      .then(res => {
         assert.equal(res.res.msg, 'hello world')
         return cc.request({ type: 1 })
-      }).then((res) => {
-        done(new Error('不应该进入正确回调，应当进入失败回调，因为retcode为1'))
-      }, (res) => {
-        assert.equal(res.retcode, 1)
-        assert.equal(res.msg, 'logout')
-        done()
       })
+      .then(
+        res => {
+          done(
+            new Error('不应该进入正确回调，应当进入失败回调，因为retcode为1')
+          )
+        },
+        res => {
+          assert.equal(res.retcode, 1)
+          assert.equal(res.msg, 'logout')
+          done()
+        }
+      )
   })
 
   it('可以reject数据', function (done) {
     class ZZ extends DB {
       constructor(options) {
         super(options)
-        this.plugin('endpoint', function () {
+
+        this.hooks.endpoint.tapPromise('endpoint', function () {
           return new Promise((resolve, reject) => {
             reject()
           })
@@ -203,13 +234,15 @@ describe('DB', function () {
       }
     }
 
-    const zz = new ZZ
+    const zz = new ZZ()
 
-    zz.request()
-      .then(() => {
+    zz.request().then(
+      () => {
         done(new Error('should not trigger resolve callback'))
-      }, () => {
+      },
+      () => {
         done()
-      })
+      }
+    )
   })
 })
